e6978b07b775bca70965857dfca8e5dcab422353
Zakaraya Ashour <ashourz@gmail.com>
2025-04-19 11:19:03 -0500

    asset client

---
+++ src/algo_royale/client/alapaca_trading/alpaca_assets_client.py
@@ -0,0 +1,51 @@
+# src/algo_royale/client/alpaca_news_client.py
+
+from typing import List, Optional
+from algo_royale.client.alpaca_base_client import AlpacaBaseClient
+from models.alpaca_trading.alpaca_asset import Asset
+from config.config import ALPACA_TRADING_URL
+
+class AlpacaAssetsClient(AlpacaBaseClient):
+    """Singleton class to interact with Alpaca's API for news data.""" 
+    
+    def __init__(self):
+        super().__init__()
+        self.base_url = ALPACA_TRADING_URL
+
+    @property
+    def client_name(self) -> str:
+        """Subclasses must define a name for logging and ID purposes"""
+        return "AlpacaAssetsClient"    
+    
+    def fetch_assets(
+            self,
+            status: Optional[str] = None,
+            asset_class: str = "us_equity",
+            exchange: Optional[str] = None
+        ) -> Optional[Asset]:
+        """Fetch asset data from Alpaca."""
+
+        params = {}
+        for k, v in {
+            "status": status,
+            "asset_class": asset_class,
+            "exchange": exchange
+        }.items():
+            if v is not None:
+                params[k] = self._format_param(v)
+                
+
+        responseJson = self._get(
+            url=f"{self.base_url}/assets",
+            params=params
+        )
+
+        self._logger.debug(f"Assets: {responseJson}")
+        if responseJson is None:
+            self._logger.warning(f"No asset data available")
+            return None       
+        
+        return Asset.parse_assets(responseJson)
+
+
+            
---
+++ tests/integration/client/alpaca_trading/test_alpaca_asset_client.py
@@ -0,0 +1,38 @@
+# src: tests/integration/client/test_alpaca_account_client.py
+
+import logging
+import pytest
+from algo_royale.client.alapaca_trading.alpaca_assets_client import AlpacaAssetsClient
+from models.alpaca_trading.alpaca_asset import Asset
+
+# Set up logging (prints to console)
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
+
+
+@pytest.fixture(scope="class")
+def alpaca_client():
+    return AlpacaAssetsClient()
+
+@pytest.mark.asyncio
+class TestAlpacaAssetClientIntegration:
+    
+    def test_fetch_assets(self, alpaca_client):
+        """Test fetching asset data from Alpaca's live endpoint."""
+
+        result = alpaca_client.fetch_assets()
+
+        assert result is not None
+        assert isinstance(result, list)
+        assert all(isinstance(asset, Asset) for asset in result)
+        assert len(result) > 0, "Expected at least one asset in the result"
+
+        expected_attrs = [
+            "id", "class_", "exchange", "symbol", "name", "status",
+            "tradable", "marginable", "maintenance_margin_requirement",
+            "shortable", "easy_to_borrow", "fractionable", "attributes"
+        ]
+
+        for asset in result:
+            for attr in expected_attrs:
+                assert hasattr(asset, attr), f"Missing expected attribute: {attr}"
