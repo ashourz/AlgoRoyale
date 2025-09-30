13ba973c7ab8f85bf108ae58f5c53835c021983c
Zakaraya Ashour <ashourz@gmail.com>
2025-09-30 10:14:13 -0500

    sql typo

--- src/algo_royale/clients/db/sql/trades/fetch_trades_by_date_range.sql
+++ src/algo_royale/clients/db/sql/trades/fetch_trades_by_date_range.sql
@@ -1,4 +1,4 @@
-SELECT id, user_id, account_id, symbol, action, settled, settlement_date, price, quantity, executed_at, created_atorder_id, updated_at FROM trades
+SELECT id, user_id, account_id, symbol, action, settled, settlement_date, price, quantity, executed_at, created_at, order_id, updated_at FROM trades
 WHERE trade_date >= %s AND trade_date <= %s
 ORDER BY trade_date
 LIMIT %s OFFSET %s;
