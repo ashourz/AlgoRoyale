c0d33ed24b0164e7d6f0f442b60f03e887f1fac4
Zakaraya Ashour <ashourz@gmail.com>
2025-09-30 09:45:35 -0500

    sql correction

--- src/algo_royale/clients/db/sql/orders/update_order_as_settled.sql
+++ src/algo_royale/clients/db/sql/orders/update_order_as_settled.sql
@@ -1,2 +1,2 @@
-UPDATE orders SET settled = 1 WHERE id = :order_id RETURNING id;
+UPDATE orders SET settled = TRUE WHERE id = :order_id RETURNING id;
 -- Update the order status to settled, indicating that the order has been fully processed and no further action is required.
